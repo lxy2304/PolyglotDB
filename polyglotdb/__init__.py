f1ea6e47800922a3b6847b1e7c177c583c73e031
lxy2304 <xiaoyili23@outlook.com>
2024-07-19 16:09:23 -0400

    Add version back

--- polyglotdb/__init__.py
+++ polyglotdb/__init__.py
@@ -1,5 +1,13 @@
 __all__ = ['query', 'io', 'corpus', 'config', 'exceptions', 'CorpusContext', 'CorpusConfig']
 
+
+__ver_major__ = 1
+__ver_minor__ = 3
+__ver_patch__ = 0
+__version__ = f"{__ver_major__}.{__ver_minor__}.{__ver_patch__}"
+
+
+
 import polyglotdb.query.annotations as graph
 
 import polyglotdb.io as io
